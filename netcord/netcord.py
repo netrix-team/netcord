2b12c3d506d87421f24e4ee52baacfeac06c717c
x4zx <shegor07@gmail.com>
2024-06-08 21:18:57 +0300

    fix docs :: netcord.py

--- netcord/netcord.py
+++ netcord/netcord.py
@@ -59,25 +59,14 @@ class Netcord(HTTPClient):
 
         Parameters
         ----------
-        session_id: str, optional
+        session_id: str
             An optional session identifier that can be included in the `state` parameter. This helps in protecting against CSRF attacks by enabling the verification of the `state` during the callback phase.
-
-        Returns
-        -------
-        str:
-            The complete authorization URL to which users should be redirected to begin the OAuth2 authorization process with Discord.
-
-        Example
-        -------
-        The URL generated will appear similar to this:
-            `https://discord.com/api/oauth2/authorize?client_id=YOUR_CLIENT_ID&redirect_uri=YOUR_REDIRECT_URI&response_type=code&scope=YOUR_SCOPES&state=RANDOM_STATE`
 
         Raises
         ------
         Exception:
             If any unexpected issues occur while generating the URL.
         """
-
 
         params = {
             'client_id': self.client_id,
@@ -103,15 +92,17 @@ class Netcord(HTTPClient):
         Detailed information about the state parameter and its usage can be found in the OAuth2 documentation:
         https://tools.ietf.org/html/rfc6749#section-10.12
 
-        Parameters:
-        - session_id (str): The session identifier associated with the state parameter.
-        - received_state (str): The state parameter received from the OAuth2 authorization callback.
+        Parameters
+        ----------
+        session_id: str
+            The session identifier associated with the state parameter.
+        received_state: str
+            The state parameter received from the OAuth2 authorization callback.
 
-        Returns:
-        - bool: Returns `True` if the state parameter is valid and matches the stored state; otherwise, raises an error.
-
-        Raises:
-        - Forbidden: If the state parameter does not exist for the given session ID, or if it does not match the received state. This indicates a potential CSRF attack or invalid session.
+        Raises
+        ------
+        Forbidden:
+            If the state parameter does not exist for the given session ID, or if it does not match the received state. This indicates a potential CSRF attack or invalid session.
         """
 
         stored_state = self.state_storage.pop(session_id, None)
@@ -133,14 +124,15 @@ class Netcord(HTTPClient):
         Detailed information about the Bearer token and Authorization header can be found in the OAuth2 documentation:
         https://tools.ietf.org/html/rfc6750
 
-        Parameters:
-        - request (Request): The HTTP request object containing the headers from which the Bearer token is to be extracted.
+        Parameters
+        ----------
+        request: Request
+            The HTTP request object containing the headers from which the Bearer token is to be extracted.
 
-        Returns:
-        - str: The extracted Bearer token as a string if the header is valid and properly formatted.
-
-        Raises:
-        - Unauthorized: If the Authorization header is missing, does not start with 'Bearer', or is improperly formatted.
+        Raises
+        ------
+        Unauthorized
+            If the Authorization header is missing, does not start with 'Bearer', or is improperly formatted.
         """
 
         header = request.headers.get('Authorization')
@@ -160,14 +152,15 @@ class Netcord(HTTPClient):
 
         This method ensures that a valid Bearer token is provided in the Authorization header of the request. It utilizes the HTTPBearer dependency to extract the token and then checks if the token corresponds to an authenticated session. If the token is missing or invalid, an Unauthorized error is raised, preventing access to the protected resource.
 
-        Parameters:
-        - bearer (HTTPAuthorizationCredentials): The Bearer token extracted from the request's Authorization header using the HTTPBearer dependency. This is automatically provided by FastAPI's dependency injection system.
+        Parameters
+        ----------
+        bearer: HTTPAuthorizationCredentials
+            The Bearer token extracted from the request's Authorization header using the HTTPBearer dependency. This is automatically provided by FastAPI's dependency injection system.
 
-        Returns:
-        - None: This method does not return any value. It either allows the request to proceed if the token is valid or raises an Unauthorized error if the token is invalid.
-
-        Raises:
-        - Unauthorized: If the Bearer token is missing, invalid, or if the token does not correspond to an authenticated session.
+        Raises
+        ------
+        Unauthorized:
+            If the Bearer token is missing, invalid, or if the token does not correspond to an authenticated session.
         """
 
         if bearer is None:
@@ -184,14 +177,15 @@ class Netcord(HTTPClient):
         Detailed information about the OAuth2 `/oauth2/@me` endpoint can be found in the Discord documentation:
         https://discord.com/developers/docs/topics/oauth2#get-current-authorization-information
 
-        Parameters:
-        - access_token (str): The access token to be verified for authentication.
+        Parameters
+        ----------
+        access_token: str
+            The access token to be verified for authentication.
 
-        Returns:
-        - bool: Returns `True` if the access token is valid and authenticated; otherwise, returns `False`.
-
-        Raises:
-        - Exception: For any unexpected errors encountered during the request execution.
+        Raises
+        ------
+        Exception:
+            For any unexpected errors encountered during the request execution.
         """
 
         headers = {'Authorization': 'Bearer ' + access_token}
@@ -217,16 +211,19 @@ class Netcord(HTTPClient):
 
         This method is responsible for exchanging a provided authorization code for an access token. The access token allows the application to make authorized requests on behalf of the user. It is a critical step in the OAuth2 authentication flow, specifically the Authorization Code Grant.
 
-        Parameters:
-        - code (str): The authorization code received from the authentication server as part of the OAuth2 authorization flow. This code is exchanged for an access token.
+        Parameters
+        ----------
+        code: str
+            The authorization code received from the authentication server as part of the OAuth2 authorization flow. This code is exchanged for an access token.
 
-        Returns:
-        - Token: An instance of the Token class containing the access token and related information. The Token class typically includes fields such as `access_token`, `token_type`, `expires_in`, and `refresh_token`.
-
-        Raises:
-        - HTTPError: If the request to exchange the authorization code for an access token fails. This could be due to an invalid code, network issues, or server-side problems.
-        - Unauthorized: If the provided authorization code is invalid or expired.
-        - Exception: For any other unexpected errors encountered during the execution.
+        Raises
+        ------
+        HTTPError:
+            If the request to exchange the authorization code for an access token fails. This could be due to an invalid code, network issues, or server-side problems.
+        Unauthorized:
+            If the provided authorization code is invalid or expired.
+        Exception:
+            For any other unexpected errors encountered during the execution.
         """
 
         data = {
@@ -243,16 +240,19 @@ class Netcord(HTTPClient):
 
         This method is responsible for renewing an access token when it has expired or is about to expire, by sending a request to the authentication server with the refresh token. The refresh token is used to obtain a new access token without requiring the user to go through the authentication flow again.
 
-        Parameters:
-        - refresh_token (str): The refresh token that was issued along with the original access token. This token is used to secure a new access token.
+        Parameters
+        ----------
+        refresh_token: str
+            The refresh token that was issued along with the original access token. This token is used to secure a new access token.
 
-        Returns:
-        - Token: An instance of the Token class containing the new access token and related information. The Token class typically includes fields such as `access_token`, `token_type`, `expires_in`, and `refresh_token`.
-
-        Raises:
-        - HTTPError: If the request to refresh the access token fails. This could be due to an invalid or expired refresh token, network issues, or server-side problems.
-        - Unauthorized: If the provided refresh token is invalid or expired.
-        - Exception: For any other unexpected errors encountered during the execution.
+        Raises
+        ------
+        HTTPError:
+            If the request to refresh the access token fails. This could be due to an invalid or expired refresh token, network issues, or server-side problems.
+        Unauthorized:
+            If the provided refresh token is invalid or expired.
+        Exception:
+            For any other unexpected errors encountered during the execution.
     """
 
         data = {
@@ -268,16 +268,19 @@ class Netcord(HTTPClient):
 
         This method sends a request to the authentication server to revoke an access token. Once revoked, the access token cannot be used for any further authenticated requests. This is an important feature for ensuring the security of the application, especially in scenarios where user sessions need to be terminated forcefully.
 
-        Parameters:
-        - access_token (str): The access token that needs to be revoked.
+        Parameters
+        ----------
+        access_token: str
+            The access token that needs to be revoked.
 
-        Returns:
-        - None: Indicates that the token has been successfully revoked.
-
-        Raises:
-        - HTTPError: If the request to revoke the access token fails. This could be due to network issues, server-side problems, or the token already being invalidated.
-        - Unauthorized: If the access token is invalid at the time of the request.
-        - Exception: For any other unexpected errors encountered during the execution.
+        Raises
+        ------
+        HTTPError:
+            If the request to revoke the access token fails. This could be due to network issues, server-side problems, or the token already being invalidated.
+        Unauthorized:
+            If the access token is invalid at the time of the request.
+        Exception:
+            For any other unexpected errors encountered during the execution.
         """
 
         data = {
@@ -294,17 +297,21 @@ class Netcord(HTTPClient):
 
         This method first validates the presence of an 'Authorization' header in the provided request, extracts the access token, and checks if the required scope 'identify' is included in the initial authorization request. If the scope is missing, it raises an exception. If the scope is present, it proceeds to fetch the user data from the endpoint that provides user information.
 
-        Parameters:
-        - request (Request): The request object from which the access token will be extracted. This object must contain an 'Authorization' header with a bearer token.
+        Parameters
+        ----------
+        request: Request
+            The request object from which the access token will be extracted. This object must contain an 'Authorization' header with a bearer token.
 
-        Returns:
-        - User: An instance of the User class populated with the user's profile data retrieved from the server. The User class typically includes fields such as `id`, `username`, `avatar`, `discriminator`, and `email`.
-
-        Raises:
-        - Unauthorized: If no authorization header is present or the token is invalid.
-        - ScopeMissing: If the required 'identify' scope is not present in the token's scope list.
-        - HTTPError: If there is an issue with the network or the server response when trying to fetch the user data.
-        - Exception: For any other unexpected errors encountered during the execution.
+        Raises
+        ------
+        Unauthorized:
+            If no authorization header is present or the token is invalid.
+        ScopeMissing:
+            If the required 'identify' scope is not present in the token's scope list.
+        HTTPError:
+            If there is an issue with the network or the server response when trying to fetch the user data.
+        Exception:
+            For any other unexpected errors encountered during the execution.
         """
 
         access_token = await self.check_token(request)
@@ -323,17 +330,21 @@ class Netcord(HTTPClient):
 
         This method fetches the profile data of a specific user by their unique user ID. It requires a bot token for authentication, which must be pre-configured. This is particularly useful in scenarios where applications need to fetch user data without direct user interaction, relying instead on bot credentials.
 
-        Parameters:
-        - user_id (str): The unique identifier for the user whose data is to be retrieved. This is a globally unique identifier that represents an individual user.
+        Parameters
+        ----------
+        user_id: str
+            The unique identifier for the user whose data is to be retrieved. This is a globally unique identifier that represents an individual user.
 
-        Returns:
-        - User: An instance of the User class populated with the detailed profile data of the user. The User class typically includes fields such as `id`, `username`, `avatar`, `discriminator`, and possibly `email` if the bot has the necessary permissions.
-
-        Raises:
-        - ValueError: If a bot token has not been set prior to invoking this method.
-        - Unauthorized: If the bot token is invalid or expired.
-        - HTTPError: If there is an issue with the network or the server response when trying to fetch the user data.
-        - Exception: For any other unexpected errors encountered during the execution.
+        Raises
+        ------
+        ValueError:
+            If a bot token has not been set prior to invoking this method.
+        Unauthorized:
+            If the bot token is invalid or expired.
+        HTTPError:
+            If there is an issue with the network or the server response when trying to fetch the user data.
+        Exception:
+            For any other unexpected errors encountered during the execution.
         """
 
         if not self.bot_token:
@@ -350,17 +361,21 @@ class Netcord(HTTPClient):
 
         This method first validates the presence of an 'Authorization' header in the provided request, extracts the access token, and checks if the required 'guilds' scope is included in the initial authorization request. If the scope is missing, it raises a ScopeMissing exception. If the scope is present, it proceeds to fetch the list of guilds from the endpoint that provides guild information associated with the user.
 
-        Parameters:
-        - request (Request): The request object from which the access token will be extracted. This object must contain an 'Authorization' header with a bearer token.
+        Parameters
+        ----------
+        request: Request
+            The request object from which the access token will be extracted. This object must contain an 'Authorization' header with a bearer token.
 
-        Returns:
-        - list[Guild]: A list of Guild objects, each containing details about a guild such as its ID, name, icon, permissions, and whether the user is an owner. This list provides a comprehensive view of the user's guild memberships.
-
-        Raises:
-        - Unauthorized: If no authorization header is present, or the token is invalid.
-        - ScopeMissing: If the required 'guilds' scope is not present in the token's scope list.
-        - HTTPError: If there is an issue with the network or the server response when trying to fetch the guild data.
-        - Exception: For any other unexpected errors encountered during the execution.
+        Raises
+        ------
+        Unauthorized:
+            If no authorization header is present, or the token is invalid.
+        ScopeMissing:
+            If the required 'guilds' scope is not present in the token's scope list.
+        HTTPError:
+            If there is an issue with the network or the server response when trying to fetch the guild data.
+        Exception:
+            For any other unexpected errors encountered during the execution.
         """
 
         access_token = await self.check_token(request)
@@ -380,14 +395,16 @@ class Netcord(HTTPClient):
 
         This method accesses the '/applications/@me' endpoint to fetch the application details of the bot. It ensures that a bot token is set; if not, it raises a ValueError. This function is useful for bot developers who need to retrieve information such as the application's ID, name, icon, and permissions.
 
-        Returns:
-        - dict: A dictionary containing key details about the application. This includes application ID, name, icon, description, and other relevant metadata that describe the bot application.
-
-        Raises:
-        - ValueError: If a bot token has not been set prior to invoking this method, indicating that the method cannot proceed without authentication.
-        - Unauthorized: If the bot token provided is invalid or expired.
-        - HTTPError: If there is an issue with the network or the server response when trying to fetch the application data.
-        - Exception: For any other unexpected errors encountered during the execution.
+        Raises
+        ------
+        ValueError:
+            If a bot token has not been set prior to invoking this method, indicating that the method cannot proceed without authentication.
+        Unauthorized:
+            If the bot token provided is invalid or expired.
+        HTTPError:
+            If there is an issue with the network or the server response when trying to fetch the application data.
+        Exception:
+            For any other unexpected errors encountered during the execution.
         """
 
         if self.bot_token is None:
